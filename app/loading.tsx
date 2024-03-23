07759a504e1b943951dafd0b216ac4629fe1bc6a
Patrick Mbugua <pkaveks2@gmail.com>
2024-03-23 12:10:52 +0300

    loader,error and not-found pages (#1)

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+const Loading = () => {
+  return (
+    <span className="loading loading-spinner loading-lg mx-auto block min-h-screen text-buttonBg"></span>
+  );
+};
+
+export default Loading;
---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div className="mx-auto block min-h-screen max-w-Xxxl place-items-center">
+      <h1 className="p-2 text-center font-titleFont text-4xl font-bold text-red-500 md:p-8 ">
+        Sorry Page Not found!
+      </h1>
+    </div>
+  );
+};
+
+export default NotFound;
